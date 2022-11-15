18c56b647d92b2ab304fe2f5bfb64fa3e969623e
Jordan Chappell <jordan.chappell33@gmail.com>
2022-11-15 17:00:57 +1100

    feat(ssh2-server): split up functions, enable VERY BASIC chat between clients

--- codebase/app.js
+++ codebase/app.js
@@ -1,73 +1,145 @@
 require('dotenv').config();
 
+const crypto = require('crypto');
 const { inspect } = require('util');
 const { Server } = require('ssh2');
 
-const server = new Server(
-  { hostKeys: [process.env.PRIVATE_KEY] }, 
-  (client) => {
-    console.log('Client connected!');
+const specialKeys = {
+  return: '\r',
+  newline: '\r\n',
+};
 
-    let username = '';
+const activeSessions = [];
 
-    client.on('authentication', (ctx) => {
-      username = ctx.username;
+const generateSessionId = () => crypto.randomUUID();
 
-      switch (ctx.method) {
-        case 'password':
-          return ctx.accept();
-        case 'publickey':
-          return ctx.accept();
-        default:
-          return ctx.reject(['keyboard-interactive', 'password', 'publickey']);
-      }
+const addNewActiveSession = (identifier, username, session, channel) => {
+  if (activeSessions.filter(s => s.identifier === identifier).length) {
+    console.log('Attempted to add an existing session to active session list');
+    console.log('identifier: ', identifier);
+    console.log('username: ', username);
+    console.log('session: ', inspect(session));
+    console.log('channel: ', inspect(channel));
+    throw Error('Something went wrong, user session already exists');
+  }
+    
+  activeSessions.push({
+    identifier,
+    username,
+    session,
+    channel
+  });
+};
+
+const removeActiveSession = (identifier) => {
+  const index = activeSessions.findIndex(s => s.identifier === identifier);
+
+  if (index < 0) {
+    console.log('Tried to remove a session that doesn\'t exist');
+    return;
+  }
+  
+  activeSessions.splice(index, 1);
+};
+
+const sendMessageToAllSessions = (fromIdentifier, message) => {
+  const sessionsToRecieve = activeSessions.filter(s => s.identifier !== fromIdentifier);
+
+  sessionsToRecieve.forEach(s => {
+    s.channel.stdout.write(`${message}${specialKeys.newline}`);
+  });
+
+  console.log(message);
+}
+
+const onClientAuth = (context) => {
+  switch (context.method) {
+    case 'password':
+      return context.accept();
+    case 'publickey':
+      return context.accept();
+    default:
+      return context.reject(['keyboard-interactive', 'password', 'publickey']);
+  }
+};
+
+const onClientReady = (client, identifier, username) => {
+  console.log('Client authenticated');
+
+  client.on('session', (accept, reject) => {
+    const session = accept();
+
+    session.on('exec', (accept, reject, info) => {
+      console.log(`Client wants to execute: ${inspect(info.command)}`);
+      const stream = accept();
+      stream.write('Sorry, no commands are currently implemented in the chat server');
+      stream.exit(0);
+      stream.end();
     });
 
-    client.on('ready', () => {
-      console.log('Client authenticated!');
+    session.on('pty', (accept, reject, info) => {
+      console.log('Client pty accepted');
+      console.log(inspect(info));
+      accept();
+    });
 
-      client.on('session', (accept, reject) => {
-        const session = accept();
+    session.on('shell', (accept, reject) => {
+      console.log('Client shell started');
 
-        session.on('exec', (accept, reject, info) => {
-          console.log(`Client wants to execute: ${inspect(info.command)}`);
-          const stream = accept();
-          stream.stderr.write('Oh no, the dreaded errors!\r');
-          stream.write('Just kidding about the errors!\r');
-          stream.exit(0);
-          stream.end();
-        });
+      const channel = accept();
 
-        session.on('pty', (accept, reject, info) => {
-          console.log('Client pty accepted');
-          accept();
-        });
+      addNewActiveSession(identifier, username, session, channel);
 
-        session.on('shell', (accept, reject) => {
-          console.log('Client shell started');
+      channel.stdout.write(`Hello there ${username}${specialKeys.newline}`);
 
-          const channel = accept();
+      let buffer = [];
 
-          channel.write(`Hello there ${username}\r`);
+      channel.on('data', (data) => {
+        const string = data.toString();
+        console.log(string);
 
-          channel.on('data', (data) => {
-            console.log(data);
-            channel.write(data);
-          });
-        });
+        if (string === specialKeys.return) {
+          sendMessageToAllSessions(identifier, buffer.join(''));
+          buffer = [];
+          return;
+        }
+
+        buffer.push(string);
       });
     });
+  });
+};
 
-    client.on('close', () => {
-      console.log('Client disconnected!');
-    });
+const onClientClose = (identifier, username) => {
+  removeActiveSession(identifier);
+  console.log(`${identifier} - ${username} closed their connection`);
+};
 
-    client.on('error', (err) => {
-      console.log(`Client error! ${err}`);
-      client.end();
-    })
-  }
-);
+const onClientError = (client, identifier, username, error) => {
+  removeActiveSession(identifier);
+  console.log(`Client ${identifier} - ${username} encountered an error: ${error.message}`);
+  client.end();
+};
+
+const onClientConnected = (client) => {
+  console.log('Client connected');
+
+  let identifier = generateSessionId();
+  let username = '';
+
+  client.on('authentication', (context) => { 
+    onClientAuth(context);
+    username = context.username;
+  });
+
+  client.on('ready', () => onClientReady(client, identifier, username));
+
+  client.on('close', () => onClientClose(identifier, username));
+
+  client.on('error', (error) => onClientError(client, identifier, username, error));
+}
+
+const server = new Server({ hostKeys: [process.env.PRIVATE_KEY] }, onClientConnected);
 
 server.listen(0, '127.0.0.1', () => {
   console.log(`Listening on port ${server.address().port}`);
